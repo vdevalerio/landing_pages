e638951cdee7d2ea3e70ef2150db0d59211b34f8
Marcos Valério <marcosvaleriox12@hotmail.com>
2024-10-17 16:27:13 -0300

    add env_example file

---
+++ institutional_model_001/env_example.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang = 'en';
+
+$company_name = 'Company';
+$footer_copy = "2024 Company - All the rights reserved.";
+
+$nav = [
+    'index' => 'Home',
+    'about' => 'About Us',
+    'contact' => 'Contact',
+    'suppliers' => 'Suppliers',
+];
+
+$logos = [
+    'logo-1' => './images/logo-1.png',
+]
+
+?>
