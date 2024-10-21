50429a98bebb9d7ba6126055752aef8f8086c4ce
Marcos Valério <marcosvaleriox12@hotmail.com>
2024-10-21 18:37:30 -0300

    improve image_slider componentization

--- institutional_model_001/components/image_slider.php
+++ institutional_model_001/components/image_slider.php
@@ -1,11 +1,16 @@
 <div class="image-slider">
     <a class="prev" onclick="changeSlide(-1)">&#10094;</a>
 
+    <?php
+        $images = getImagesFromDir($imageSlider['imagesPath']);
+    ?>
+
     <div class="image-slider-slides">
-        <?php foreach ($logos as $logo): ?>
+        <?php foreach ($images as $image): ?>
             <div class="image-slider-slide">
-                <img class="image-slider-image" src="<?php echo htmlspecialchars($logo['path']); ?>"
-                    alt="<?php echo htmlspecialchars($logo['title']); ?>"
+                <img class="image-slider-image"
+                src="<?php echo htmlspecialchars($image['path']); ?>"
+                alt="<?php echo htmlspecialchars($image['title']); ?>"
                 >
             </div>
         <?php endforeach; ?>
