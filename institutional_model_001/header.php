3cad35e6784079bb7afe2e95168ca5553c3238af
Marcos Valério <marcosvaleriox12@hotmail.com>
2024-10-21 17:58:40 -0300

    standardize header

--- institutional_model_001/header.php
+++ institutional_model_001/header.php
@@ -7,27 +7,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $companyName ?></title>
-    <link rel="icon" type="image/x-icon" href="<?php echo $logos['main']['path'] ?>">
+    <link rel="icon" type="image/x-icon" href="<?php echo $faviconPath ?>">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
     <header>
         <div class="header-container">
-            <img class="header-image" src="<?php echo $logos['main']['path'] ?>" alt="<?php echo $logos['main']['title'] ?>">
+            <img class="header-image" src="<?php echo $nav['imagePath'] ?>"
+            alt="<?php echo $logos['main']['title'] ?>"
+            >
             <nav class="menu-items">
-                <a href="index.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>">
-                    <?php echo $nav['index'] ?>
-                </a>
-                <a href="about.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'about.php' ? 'active' : ''; ?>">
-                    <?php echo $nav['about'] ?>
-                </a>
-                <a href="contact.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'contact.php' ? 'active' : ''; ?>">
-                    <?php echo $nav['contact'] ?>
-                </a>
-                <a href="suppliers.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'suppliers.php' ? 'active' : ''; ?>">
-                    <?php echo $nav['suppliers'] ?>
-                </a>
+                <?php foreach ($nav['items'] as $ref => $item): ?>
+                    <a href="<?php echo htmlspecialchars($ref); ?>"
+                    class="<?php echo basename($_SERVER['PHP_SELF']) == $ref ? 'active' : ''; ?>">
+                        <?php echo htmlspecialchars($item); ?>
+                    </a>
+               <?php endforeach; ?>
             </nav>
         </div>
     </header>
