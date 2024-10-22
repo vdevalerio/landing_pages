314c647352a629c6935b79ae0ab4b833c592b8b0
Marcos Valério <marcosvaleriox12@hotmail.com>
2024-10-21 21:37:36 -0300

    Updates documentation

--- institutional_model_001/README.md
+++ institutional_model_001/README.md
@@ -1,2 +1,9 @@
 # Instructions
 Create a `env.php` file, following the example of `env_example.php` to setup your variables to be used along the system.
+
+Create image files structure like:
+- images
+    - card_slider
+        - Files here should be on this format: `prefixN-image title`.
+    - image_slider:
+        - Files here should be on this format: `prefixN-image title`.
--- institutional_model_001/env_example.php
+++ institutional_model_001/env_example.php
@@ -2,21 +2,109 @@
 
 $lang = 'en';
 
-$companyName = 'Company';
-$footerCopy = "2024 Company - All the rights reserved.";
+$companyName = 'Company Name';
+$faviconPath = './imagesExample/favicon.png';
 
 $nav = [
-    'index' => 'Home',
-    'about' => 'About Us',
-    'contact' => 'Contact',
-    'suppliers' => 'Suppliers',
-];
-
-$logos = [
-    'logo1' => [
-        'title' => 'Company Name',
-        'path' =>'./images/logo.png',
+    'imagePath' => './imagesExample/nav_image.png',
+    'items' => [
+        'index.php' => 'Home',
+        'about.php' => 'About Us',
+        'contact.php' => 'Contact',
+        'suppliers.php' => 'Suppliers',
     ],
 ];
 
+$imageSlider = [
+    'imagesPath' => './imagesExample/image_slider',
+];
+
+$textImage = [
+    'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed sed elit volutpat, porttitor dolor at, porttitor nulla.
+        Suspendisse rutrum faucibus mollis. Phasellus at tempor nisi, eu porttitor libero.
+        Aenean tempus felis sed velit consectetur, nec euismod urna eleifend.
+        Integer elementum est sit amet ornare laoreet.
+        Donec elementum sed arcu sed vulputate. Fusce vehicula et erat et convallis.
+        Aenean egestas nisl blandit scelerisque egestas. Sed sed molestie libero.
+        Vestibulum id enim quis ex commodo bibendum.
+        Curabitur suscipit mauris orci, laoreet cursus est varius et.
+        Pellentesque vel porta felis. Morbi posuere at enim nec ultrices.
+        Sed blandit faucibus metus, eget fermentum arcu fringilla eu.
+        Pellentesque feugiat metus id eros rhoncus, sed finibus ipsum dapibus.
+        Mauris ullamcorper porta diam, vitae ornare risus luctus lacinia.',
+    'image' => [
+        'title' => 'Title',
+        'path' => './imagesExample/favicon.png'
+    ],
+    'button' => [
+        'link' => 'about.php',
+        'text' => 'More',
+    ]
+];
+
+$cardSlider = [
+    // Info
+    'title' => 'Slider Title',
+
+    //  Texts should contain labels for the same amount of images contained in imagesPath.
+    'texts' => [
+        'Card 1 Text',
+        'Card 2 Text',
+        'Card 3 Text',
+        'Card 4 Text',
+        'Card 5 Text',
+        'Card 6 Text',
+        /**
+         * .
+         * .
+         * .
+         * Card N Text
+         */
+    ],
+    'buttons' => [
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        [
+            'link' => 'about.php',
+            'text' => 'More',
+        ],
+        /**
+         * .
+         * .
+         * .
+         * [
+         *  'link' => 'Link Button N',
+         *  'text' => 'Text Button N
+         * ]
+         */
+    ],
+    'imagesPath' => './imagesExample/card_slider',
+
+    // Controllers
+    'visibleCards' => 3,
+    'gapWidthPercentage' => 5,
+    'transitionDelayMS' => 500,
+    'sliderIntervalMS' => 5000,
+];
+
+$footerCopy = "20XX {$companyName} - All rights reserved.";
 ?>
